eac735dd080d710e69beeb64545c79c04ddaa856
Karl Broman <kbroman@gmail.com>
2016-11-10 15:25:30 -0600

    Add function read_file()
     - reads csv, json, or rds file (according to file extension)

---
+++ R/read_file.R
@@ -0,0 +1,26 @@
+#' Read a file
+#'
+#' Read a file that is either RDS, CSV, or JSON, with the method determined by the file extension.
+#'
+#' @param file Character string of input file path
+#' @param ... Passed to whatever function it calls
+#'
+#' @importFrom qtl2geno read_csv
+#' @importFrom jsonlite fromJSON
+#' @export
+#'
+#' @examples
+#' \dontrun{x <- read_file("myfile.rds")}
+#' \dontrun{y <- read_file("myfile.csv")}
+#' \dontrun{z <- read_file("myfile.json")}
+read_file <-
+    function(file, ...)
+{
+    tmp <- strsplit("\\.", file)[[1]]
+    file_extension <- tmp[length(tmp)]
+    switch(file_extension,
+           "csv" = qtl2geno::read_csv(file, ...),
+           "rds" = readRDS(file),
+           "json" = jsonlite::fromJSON(scan(file, what=character())),
+           stop("Can't handle file extension ", file_extension))
+}
