81113bb19d0e815fcf75bc8db2b580f5b9956050
Karl Broman <kbroman@gmail.com>
2016-11-10 21:15:22 -0600

    Add function run_scan1
     - Load data from a set of files and the run scan1
     - write to an RDS file or print

---
+++ R/run_scan1.R
@@ -0,0 +1,56 @@
+#' Run scan1
+#'
+#' Read in a bunch of data and then run \code{\link[qtl2scan]{scan1}}.
+#'
+#' @param genoprobs_file Name of file with genotype probabilities
+#' @param pheno_file Name of file with phenotypes
+#' @param output_file Optional output RDS file. If NULL, print output as a table.
+#' @param kinship_file Optional file containing kinship matrix
+#' @param addcovar_file Optional file containing additive covariates
+#' @param Xcovar_file Optional file containing X chromosome covariates
+#' @param intcovar_file Optional file containing interactive covariates
+#' @param weights_file Optional file containing covariates
+#' @param reml If TRUE, use REML; otherwise, use maximum likelihood
+#' @param cores Number of CPU cores to use
+#'
+#' @importFrom qtl2convert scan_qtl2_to_qtl
+#' @export
+run_scan1 <-
+    function(genoprobs_file,
+             pheno_file,
+             output_file=NULL,
+             kinship_file=NULL,
+             addcovar_file=NULL,
+             Xcovar_file=NULL,
+             intcovar_file=NULL,
+             weights_file=NULL,
+             reml=TRUE,
+             cores=1)
+{
+    # read file if not NULL; otherwise pass along the NULL
+    read_file0 <-
+        function(filename)
+        {
+            if(is.null(filename) || filename=="") return(NULL)
+            else return(read_file(filename))
+        }
+
+    result <- scan1(genoprobs=read_file(genoprobs_file),
+                    pheno=read_file(pheno_file),
+                    kinship=read_file0(kinship_file),
+                    addcovar=read_file0(addcovar_file),
+                    Xcovar=read_file0(Xcovar_file),
+                    intcovar=read_file0(intcovar_file),
+                    weights=read_file0(weights_file),
+                    reml=reml, cores=cores)
+
+    if(is.null(output_file)) {
+        tab <- qtl2convert::scan_qtl2_to_qtl(result)
+        print( cbind(marker=rownames(tab),
+                     as.data.frame(tab)),
+              row.names=FALSE)
+    }
+    else {
+        saveRDS(result, file=output_file)
+    }
+}
