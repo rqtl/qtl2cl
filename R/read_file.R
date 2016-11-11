5cce926624fee26e29461d0efefd16d972569ab5
Karl Broman <kbroman@gmail.com>
2016-11-10 20:39:42 -0600

    read_file: also read yaml files

--- R/read_file.R
+++ R/read_file.R
@@ -1,18 +1,32 @@
 #' Read a file
 #'
-#' Read a file that is either RDS, CSV, or JSON, with the method determined by the file extension.
+#' Read a file that is either RDS, CSV, JSON, or YAML, with the method
+#' determined by the file extension.
 #'
-#' @param file Character string of input file path
+#' @param file Character string of input file; must have file
+#' extension \code{.rds}, \code{.csv}, \code{.json}, or \code{.yaml}.
 #' @param ... Passed to whatever function it calls
 #'
-#' @importFrom qtl2geno read_csv
+#' @return Could be most anything; whatever gets read in by the
+#' corresponding R function for the file type. See Details.
+#'
+#' @details For CSV files, we use \code{\link[qtl2geno]{read_csv}}, which calls
+#' \code{\link[data.table]{fread}} with a particular set of options.
+#' For RDS files, we use \code{\link[base]{readRDS}}. For JSON files,
+#' we use \code{\link[base]{readLines}} and
+#' \code{\link[jsonlite]{fromJSON}}. For YAML files, we use
+#' \code{\link[yaml]{yaml.load_file}}.
+#'
 #' @importFrom jsonlite fromJSON
+#' @importFrom yaml yaml.load_file
+#' @importFrom qtl2geno read_csv
 #' @export
 #'
 #' @examples
-#' \dontrun{x <- read_file("myfile.rds")}
-#' \dontrun{y <- read_file("myfile.csv")}
-#' \dontrun{z <- read_file("myfile.json")}
+#' \dontrun{w <- read_file("myfile.rds")
+#' x <- read_file("myfile.csv")
+#' y <- read_file("myfile.json")
+#' z <- read_file("myfile.yaml")}
 read_file <-
     function(file, ...)
 {
@@ -22,5 +36,6 @@ read_file <-
            "csv" = qtl2geno::read_csv(file, ...),
            "rds" = readRDS(file),
            "json" = jsonlite::fromJSON( readLines(file) ),
+           "yaml" = yaml::yaml.load_file(filename),
            stop("Can't handle file extension ", file_extension))
 }
